b6eab5a08e25dc6d414b2ad4a58ee0cffd930cae
Lovedeep ViRk <73482922+Lovedeep-ViRk@users.noreply.github.com>
2020-11-07 07:39:41 +0530

    Update __main__.py

--- cinderella/__main__.py
+++ cinderella/__main__.py
@@ -180,7 +180,7 @@ def send_start(bot, update):
     first_name = update.effective_user.first_name 
     text = PM_START_TEXT
 
-    keyboard = [[InlineKeyboardButton(text="🤝Help",callback_data="help_back"),InlineKeyboardButton(text="👑Creator👨‍🔧",url="https://t.me/Lovedeep-ViRk")]]
+    keyboard = [[InlineKeyboardButton(text="🤝Help",callback_data="help_back"),InlineKeyboardButton(text="👑Creator👨‍🔧",url="https://t.me/Lovedeep_ViRk")]]
     keyboard += [[InlineKeyboardButton(text="🔶Connect Group🏘", callback_data="main_connect"),InlineKeyboardButton(text="⚜️Add Me⚜️",url="t.me/{}?startgroup=true".format(bot.username))]]
 
     update.effective_message.reply_photo(img, PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_NAME, OWNER_ID), 
